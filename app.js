0d73aedc9d28a0984a393bfe0763f89e6345a3f3
SpagettiesWeb <bruhzamn10@gmail.com>
2026-03-19 23:09:16 +0300

    №4 - №5

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-// Дано число. Выведите в консоль сумму первой и последней цифры этого числа.
+// Дано число. Выведите количество цифр в этом числе.
+const num = 1512;
+console.log(String(num).length);
 
-const num = 1252;
-const digit = String(Math.abs(num));
+// Даны два числа. Проверьте, что первые цифры этих чисел совпадают.
 
-console.log(Number(digit[0]) + Number(digit.at(-1)));
+const num1 = 2141;
+const num2 = 4353;
+console.log(String(num1)[0] === String(num2)[0]);
+
