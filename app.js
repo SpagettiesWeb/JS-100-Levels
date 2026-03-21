3e2afaacf294c42166b582669ff0d887e3685666
SpagettiesWeb <bruhzamn10@gmail.com>
2026-03-21 16:01:55 +0300

    №1 - №3

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-// Заполните массив целыми числами от 1 до 10.
-const arr = [];
-for (let i = 1; i <= 10; i++) {
-    arr.push(i);
-}
-console.log(arr);
+// Дан массив со строками. Оставьте в этом массиве только те строки, которые начинаются на http://
+const arr = [
+    'http://a.co', 'http://x.io',
+    'http://q.gs', 'http://t.ly',
+    'Any', 'Rofls', 'Kek',
+    'http://u.nu', 'http://v.gd',
+    'Something'
+];
 
-// Заполните массив четными числами из промежутка от 1 до 100.
-const arr2 = [];
-for (let i = 1; i <= 100; i++) {
-    if (i % 2 === 0) {
-        arr2.push(i);
-    }
-}
-console.log(arr2);
+const arrLinks = arr.filter(str => str.startsWith('http://'));
+console.log(arrLinks);
 
-// Округлите эти дроби до одного знака в дробной части.
-const arr3 = [1.456, 2.125, 3.32, 4.1, 5.34];
-const arr3Rounded = arr3.map(el => Number(el.toFixed(1)));
-console.log(arr3Rounded);
+// Дан массив со строками. Оставьте в этом массиве только те строки, которые заканчиваются на .html.
+const arr2 = [
+  "index.html", "42",
+  "about.html", "7",
+  "contact.html", "100",
+  "blog.html", "3.14",
+  "portfolio.html", "-5"
+];
+const arr2Html = arr2.filter(str => str.endsWith('.html'));
+console.log(arr2Html);
+
+// Дан массив с числами. Увеличьте каждое число из массива на 10 процентов.
+const numsArr = [1,2,3,4,5,6,7,8,9,10];
+const numsArrPercent = numsArr.map(num => Number((num + num /10).toFixed(1)));
+console.log(numsArrPercent);
